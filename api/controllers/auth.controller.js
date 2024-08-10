48b2d631a9f2fcb83c9fb25e016b15d44fc88735
deepak04iiitn <dky422003@gmail.com>
2024-08-11 03:19:21 +0530

    completed signup functionality

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,145 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+
+export const signup = async(req , res , next) => {              // we use async when we are taking/fetching some data from the database
+
+    const { username , email , password } = req.body;      // getting them separately from req.body
+
+    if(!username || !email || !password || username === '' || email === '' || password === '')
+        {
+            next(errorHandler(400 , 'All fields are required'));
+        }
+
+        const hashedPassword = bcryptjs.hashSync(password , 10);
+
+        const newUser = new User({
+            username,
+            email,
+            password : hashedPassword,
+        })
+
+        try {
+            await newUser.save();
+            res.json('Signup successfull');
+        } catch (error) {
+            next(error);
+        }
+}
+
+
+export const signin = async(req , res , next) => {
+
+    const { email , password } = req.body;
+
+    if(!email || !password || email === '' || password === '')
+        {
+            next(errorHandler(400 , 'All fields are required'));
+        }
+
+    try {
+        
+        const validUser = await User.findOne({ email });
+
+        if(!validUser)
+            {
+                return next(errorHandler(404 , 'Wrong Credentials!'));
+            }
+
+        const validPassword = bcryptjs.compareSync(password , validUser.password);
+
+        if(!validPassword)
+            {
+                return next(errorHandler(400 , 'Wrong Credentials!'));
+            }
+
+
+        // now if everything is correct both email and password , we need to authenticate our user
+        // we will do it by a package called json web token
+
+        const token = jwt.sign(
+            {
+                id : validUser._id,
+            },
+            process.env.JWT_SECRET
+        );
+
+        const { password : pass , ...rest } = validUser._doc;       // we do not want to send back the password , thats why we are separating it 
+
+        res.status(200).cookie('access_token' , token , {
+            httpOnly : true
+        }).json(rest);
+
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export const google = async(req , res , next) => {
+
+    // if the user exists , we will sign in the user
+    // if the user does not exist we will create the new user
+    // we are getting 3 infos from the frontend :- name , email , googlePhotoUrl
+    // things we are sending from frontend , in backend we will get them in req.body
+
+    const {name , email } = req.body;
+
+    try {
+
+        const user = await User.findOne({email});
+        if(user)                                                   // user does exist , so just signing in
+            {
+                const token = jwt.sign(                            // token part is must to sign in after we successfully create the user
+                    {
+                        id : user._id,
+                    },
+                    process.env.JWT_SECRET
+                );
+
+                const { password , ...rest } = user._doc;
+
+                res.status(200).cookie('access_token' , token , {
+                    httpOnly : true
+                }).json(rest);
+            }
+            else
+            {
+                // now if the user does not exist we have to create a new user
+                // for creating a new user , we will require username , email , password
+                // we can create username from the displayName we are getting from the google , email we are already getting
+                // for the pssword , we will generate a random password , that user can change later
+
+                const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+
+                const hashedPassword = bcryptjs.hashSync(generatedPassword , 10);
+
+                const newUser = new User({
+                    username : name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),         // Deepak Yadav => deepakyadav5358
+                    email,
+                    password : hashedPassword,
+                });
+
+                await newUser.save();
+
+                const token = jwt.sign(
+                    {
+                        id : newUser._id,
+                    },
+                    process.env.JWT_SECRET
+                );
+
+                const { password , ...rest } = newUser._doc;
+
+                res.status(200).cookie('access_token' , token , {
+                    httpOnly : true
+                }).json(rest);
+
+            }
+
+    } catch (error) {
+        next(error);
+    }
+}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,105 @@
-import React from 'react'
+import { Button, Label, TextInput } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'; 
 
 export default function SignUp() {
+
+    const [formData , setFormData] = useState({});
+    const [errorMessage , setErrorMessage] = useState(null);
+    const [loading , setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        //console.log(e.target.value);
+
+        setFormData({...formData , [e.target.id] : e.target.value.trim() });     // spreading the formData bcz when we will be changing the email/password , we want to keep the previous input field information like of username
+
+    }
+
+    const handleSubmit = async(e) => {          // as we want to submit to database and it takes time so we will use async and await
+        
+        e.preventDefault();
+
+        if(!formData.username || !formData.email || !formData.password)
+            {
+                return setErrorMessage('Please fill out all fields!');
+            }
+
+        try {
+            setLoading(true);
+            setErrorMessage(null);
+
+            const res = await fetch('/api/auth/signup' , {
+                method : 'POST',
+                headers : { 'Content-Type' : 'application/json' },
+                body : JSON.stringify(formData),
+            });
+            const data = await res.json();
+
+            if(data.success === false)
+                {
+                    return setErrorMessage(data.message);
+                }
+            
+            setLoading(false);
+
+            if(res.ok)
+                {
+                    navigate('/sign-in');
+                }
+
+        } catch (error) {
+            setErrorMessage(error.message);
+            setLoading(false);
+        }
+
+    }
+
+
   return (
-    <div>SignUp</div>
-  )
+    <div className='flex justify-center items-center h-screen'>
+      <form className='flex flex-col gap-4 w-[30%]' onSubmit={handleSubmit}>
+        <div>
+          <Label value='Your username' />
+          <TextInput
+            type='text'
+            placeholder='Username'
+            id='username'
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <Label value='Your email' />
+          <TextInput
+            type='email'
+            placeholder='name@company.com'
+            id='email'
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <Label value='Your password' />
+          <TextInput
+            type='password'
+            placeholder='********'
+            id='password'
+            onChange={handleChange}
+          />
+        </div>
+
+        <Button gradientDuoTone='purpleToPink' type='submit'>
+          Sign Up
+        </Button>
+
+        <div className='flex gap-2 text-sm mt-2 justify-center'>
+          <span>Have an account?</span>
+          <Link to='/sign-in' className='text-blue-500'>
+            Sign In
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
